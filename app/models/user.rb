68fad1053dd1b34c2d2dfba0761d40acbc4577a0
Moresh <moreshk@gmail.com>
2013-07-08 21:53:52 -0500

    8

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -27,4 +28,10 @@ class User < ActiveRecord::Base
 
   validates :phone, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :city, presence: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
